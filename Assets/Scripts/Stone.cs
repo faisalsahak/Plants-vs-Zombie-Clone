fec9948cea6f587441740d20a41d217cf6a4087a
user <user@users-MacBook-Pro.local>
2020-02-17 00:24:55 -0800

    aded a script for the stone sprites

--- Assets/Scripts/Stone.cs
+++ Assets/Scripts/Stone.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using UnityEngine;
+using System.Collections;
+
+public class Stone : MonoBehaviour {
+	
+	private Animator animator;
+	
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+	
+	void Update () {
+		
+	}
+	
+	void OnTriggerStay2D (Collider2D collider) {
+		Attacker attacker = collider.gameObject.GetComponent<Attacker>();
+		
+		if (attacker) {
+			animator.SetTrigger ("underAttack trigger");
+		}
+	}
+}
